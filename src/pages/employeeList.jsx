4bfbbc559ed0fc8ac253fb3f334b8efe2a34b418
NicolasSIO <nicolas.mazaud.sio@gmail.com>
2023-12-07 18:02:09 +0100

    Change table library + filter + pagination

--- src/pages/employeeList.jsx
+++ src/pages/employeeList.jsx
@@ -1,30 +1,91 @@
 import React from "react";
-import "react-data-grid/lib/styles.css";
-import DataGrid from "react-data-grid";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
+import DataTable from "react-data-table-component";
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
 import "./employeeList.css";
 
 const EmployeeList = () => {
   const navigate = useNavigate();
-  const employees = useSelector((state) => state.employee.employee);
+
+  const data = useSelector((state) => state.employee.employee);
 
   const columns = [
-    { key: "firstName", name: "First Name" },
-    { key: "lastName", name: "Last Name" },
-    { key: "dateStart", name: "Start Date" },
-    { key: "department", name: "Department" },
-    { key: "birthDate", name: "Date of Birth" },
-    { key: "street", name: "Street" },
-    { key: "city", name: "City" },
-    { key: "state", name: "State" },
-    { key: "zipCode", name: "Zip Code" },
+    {
+      name: "First Name",
+      selector: (row) => row.firstName,
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Last Name",
+      selector: (row) => row.lastName,
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Start Date",
+      selector: (row) => row.dateStart,
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Department",
+      selector: (row) => row.department,
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Date of Birth",
+      selector: (row) => row.birthDate,
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Street",
+      selector: (row) => row.street,
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "City",
+      selector: (row) => row.city,
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "State",
+      selector: (row) => row.state,
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Zip Code",
+      selector: (row) => row.zipCode,
+      sortable: true,
+      wrap: true,
+    },
   ];
+
+  const tableData = {
+    columns,
+    data,
+  };
 
   return (
     <main>
       <h1>Current Employees</h1>
-      <DataGrid className="table" columns={columns} rows={employees} />
+      <DataTableExtensions print={false} export={false} {...tableData}>
+        <DataTable
+          columns={columns}
+          data={data}
+          defaultSortField="id"
+          defaultSortAsc={false}
+          pagination
+          highlightOnHover
+        />
+      </DataTableExtensions>
       <button onClick={() => navigate("/")}>Home</button>
     </main>
   );
